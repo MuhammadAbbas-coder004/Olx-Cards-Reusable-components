b757eed0e2aef3cf2893d2cb38c5acdec185119a
MuhammadAbbas-coder004 <mabbasmshahi2003@gmail.com>
2025-11-30 22:04:56 +0500

    add

--- olxcards/src/components/Card.jsx
+++ olxcards/src/components/Card.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+  console.log(props);
+  
   return (
     
         <div className="card">
           <img
-            src="https://images.pexels.com/photos/358070/pexels-photo-358070.jpeg"
+            src={props.img}
             alt="Civic"
             className="product-img"
           />
 
-          <h1 className="card-title">Honda Civic 2020</h1>
-          <p className="card-desc">Total Genuine • Family Used</p>
-          <h3 className="card-color">Color: Black</h3>
-          <h2 className="card-price">Rs 5,200,000</h2>
-          <h6 className="card-location">Lahore</h6>
+          <h1 className="card-title"> {props.productname} </h1>
+          <p className="card-desc">  {props.category} </p>
+          <h3 className="card-color"> {props.color} </h3>
+          <h2 className="card-price">{props.price}</h2>
+          <h6 className="card-location"> {props.location}  </h6>
         </div>
   )
 }
